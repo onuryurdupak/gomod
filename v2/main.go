96ee19616647eed6ce867781c3a38652fed23bf9
Onur YURDUPAK <onuryurdupak@gmail.com>
2023-02-26 20:53:30 +0300

    Update main.go

--- v2/main.go
+++ v2/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"github.com/google/uuid"
 	"github.com/onuryurdupak/gomod/v2/stdout"
 )
 
 func main() {
-	stdout.PrintfStyled("<red>Hello</red> <yellow>World</yellow><green>!</green>")
+	stdout.SetOutputLevel(stdout.OutputLevelDebug)
+	stdout.Output(stdout.OutputLevelDebug, "Starting application.")
+	stdout.PrintfStyled("<red>Hello</red> <yellow>World</yellow><green>!</green>\n")
+	stdout.OutputWID(stdout.OutputLevelInfo, uuid.NewString(), "Shutting down application.")
 }
